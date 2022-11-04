0d4b6c8c94fa9ecfca2075a1b04314eaabf5eb3e
Haran Raajesh <haran@Harans-MacBook-Pro.local>
2022-11-04 16:19:10 +0530

    Pressure code

--- Pressure_sensor/Pressure_sensor.ino
+++ Pressure_sensor/Pressure_sensor.ino
@@ -1,6 +1,7 @@
-int Sensor_pin = A3; // the analog pin connected to the sensor
+int Sensor_pin = 0; // the analog pin connected to the sensor
 int LED_pin = 10; // pin connected to LED
 //int THRESHOLD = 100;
+int value;
 void setup()
 {
   pinMode(LED_pin, OUTPUT);
@@ -8,7 +9,7 @@ void setup()
 }
 void loop()
 {
-int value = analogRead(Sensor_pin);  // function to read analog voltage from sensor
+value = analogRead(Sensor_pin);  // function to read analog voltage from sensor
 Serial.print("sensor value is: ");
 Serial.println(value);
 delay(500); // to make the LED visible
