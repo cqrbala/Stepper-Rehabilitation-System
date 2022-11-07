0fec5d60b9e08b7d7282542ea5b8557f66f83c2c
arnavmago <arnavmago@gmail.com>
2022-11-07 14:32:27 +0530

    Stepper code altered

--- Pressure_sensor/Pressure_sensor.ino
+++ Pressure_sensor/Pressure_sensor.ino
@@ -1,10 +1,9 @@
-int Sensor_pin = 0; // the analog pin connected to the sensor
+int Sensor_pin = 4; // the analog pin connected to the sensor
 int LED_pin = 10; // pin connected to LED
 //int THRESHOLD = 100;
 int value;
 void setup()
 {
-  pinMode(LED_pin, OUTPUT);
   Serial.begin(9600);
 }
 void loop()
@@ -12,5 +11,5 @@ void loop()
 value = analogRead(Sensor_pin);  // function to read analog voltage from sensor
 Serial.print("sensor value is: ");
 Serial.println(value);
-delay(500); // to make the LED visible
+delay(500); 
 }
