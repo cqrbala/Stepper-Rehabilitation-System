cd203322fe62bcd6be6c8803d25f154b28ab7d1b
arnavmago <arnavmago@gmail.com>
2022-11-11 00:06:35 +0530

    Added ThingSpeak functionality

--- Pressure_sensor/Pressure_sensor.ino
+++ Pressure_sensor/Pressure_sensor.ino
@@ -1,15 +1,59 @@
-int Sensor_pin = 4; // the analog pin connected to the sensor
-int LED_pin = 10; // pin connected to LED
-//int THRESHOLD = 100;
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <WiFi.h>
+#include <ThingSpeak.h>
+
+char ssid[] = "Arnav";
+char password[] = "pranjal13";
+
+const char* server = "mqtt3.thingspeak.com";
+char mqttUserName[] = "mwa0000026745613";
+char mqttPass[] = "THZAMM30249U49OK"; //Change to your MQTT API key from Account > MyProfile on Thingspeak.
+int writeChannelID = 1925729;
+char writeAPIKey[] = "OLVQ0WA9GL4CLREE";
+char readAPIKey[] = "M5SLOVSH2YRWMURX";
+
+WiFiClient client; // Instantiating WiFiClient object
+PubSubClient mqttClient(server, 1883, client); // Instantiating Publisher Sub-Client Object
+
+int Sensor_pin = 32; // the analog pin connected to the sensor
 int value;
+
 void setup()
 {
+  WiFi.begin(ssid, password);
   Serial.begin(9600);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Not connected to WiFi.");
+  }
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 void loop()
 {
-value = analogRead(Sensor_pin);  // function to read analog voltage from sensor
-Serial.print("sensor value is: ");
-Serial.println(value);
-delay(500); 
+  while(mqttClient.connected() == NULL)
+  {
+    Serial.println("MQTT not connected");
+    Serial.println(mqttClient.connect("KSYxFSI0BicuKikKEyYaEjU","KSYxFSI0BicuKikKEyYaEjU","Na4N9kkuVLSjuO1xucSvUd60"));
+  }
+  Serial.println("MQTT connected");
+  mqttClient.loop(); // maintain connection
+
+  value = analogRead(Sensor_pin);  // function to read analog voltage from sensor
+  Serial.print("sensor value is: ");
+  Serial.println(value);
+  mqttPublish(writeChannelID, value);
+  delay(500); 
+}
+
+void mqttPublish(long pubChannelID, int force) {
+  String topic, data;
+  topic = "channels/" + String(pubChannelID) + "/publish";
+  data = "field3=" + String(force);
+  Serial.println(data);
+  mqttClient.publish(topic.c_str(), dataString.c_str());
 }
--- Stepper_code/Stepper_code.ino
+++ Stepper_code/Stepper_code.ino
@@ -1,6 +1,25 @@
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <WiFi.h>
+#include <ThingSpeak.h>
+
 #define trigger_pin 2
 #define echo_pin 4
 #define sound_speed 343
+int Sensor_pin = 0;
+
+char ssid[] = "Arnav";
+char password[] = "Pranjal13";
+
+const char* server = "mqtt3.thingspeak.com";
+char mqttUserName[] = "mwa0000026745613";
+char mqttPass[] = "THZAMM30249U49OK"; //Change to your MQTT API key from Account > MyProfile on Thingspeak.
+int writeChannelID = 1925729;
+char writeAPIKey[] = "OLVQ0WA9GL4CLREE";
+char readAPIKey[] = "M5SLOVSH2YRWMURX";
+
+WiFiClient client; // Instantiating WiFiClient object
+PubSubClient mqttClient(server, 1883, client); // Instantiating Publisher Sub-Client Object
 
 // inital positon of the step which we will use as a point of reference
 double origin;
@@ -20,13 +39,35 @@ int sign_flag = 0;
 // Min displacement required to complete a step
 double step_threshold = 0.03;
 
+double duration, distance, step_displacement, sensorValue, millivolt;
+
 void setup() {
   pinMode(trigger_pin, OUTPUT);
   pinMode(echo_pin, INPUT);
+  WiFi.begin(ssid, password);
   Serial.begin(115200);
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Not connected to WiFi.");
+  }
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setting up MQTT Broker details
+  mqttClient.setServer(server, 1883);
 }
 
-void loop() {
+void loop()  {
+  while(mqttClient.connected() == NULL)
+  {
+    Serial.println("MQTT not connected");
+    Serial.println(mqttClient.connect("KSYxFSI0BicuKikKEyYaEjU","KSYxFSI0BicuKikKEyYaEjU","Na4N9kkuVLSjuO1xucSvUd60"));
+  }
+  Serial.println("MQTT connected");
+  mqttClient.loop(); // maintain connection
 
   digitalWrite(trigger_pin, 1);
   delayMicroseconds(10);
@@ -38,35 +79,35 @@ void loop() {
     origin = pulseIn(echo_pin, 1);
     origin = (origin / 2000000)*sound_speed;
     origin_flag = 1;
-//    Serial.print("Origin set as: ");
-//    Serial.println(origin);
+    // Serial.print("Origin set as: ");
+    // Serial.println(origin);
   }
   else {
 
     // Processing of the ultrasonic sensor's input
-    double duration, distance,step_displacement;
+    
     duration = pulseIn(echo_pin, 1);
     duration = duration / 2000000; // now duration is in seconds
     distance = duration*sound_speed;
 
-//    Serial.print("Ultrasonic sensor data is: ");
-//    Serial.println(distance);
-//    Serial.print("sign flag is: ");
-//    Serial.println(sign_origin);
+    // Serial.print("Ultrasonic sensor data is: ");
+    // Serial.println(distance);
+    // Serial.print("sign flag is: ");
+    // Serial.println(sign_origin);
 
-//    Reading EMG sensor input (gets the voltage in milliVolts)
-    float sensorValue = analogRead(0);
-    sensorValue = analogRead(0);
-    float millivolt = (sensorValue/1023)*5;
+    // Reading EMG sensor input (gets the voltage in milliVolts)
+    sensorValue = analogRead(Sensor_pin);
+    sensorValue = analogRead(Sensor_pin);
+    millivolt = (sensorValue/1023)*5;
 
     Serial.print("Muscle sensor value: ");
     Serial.println(sensorValue);
-    Serial.print("millivolts: ");
-    Serial.println(millivolt);
+    // Serial.print("millivolts: ");
+    // Serial.println(millivolt);
  
     step_displacement = distance - origin;
-//    Serial.print("Distance is: ");
-//    Serial.println(step_displacement);
+    // Serial.print("Distance is: ");
+    // Serial.println(step_displacement);
     
     
     if(abs(step_displacement) >= step_threshold) {
@@ -75,18 +116,31 @@ void loop() {
         if(step_displacement < 0) sign_origin = 1;
         else sign_origin = -1;
         num_steps += 1;
-//        Serial.print("One small step for Man, one large leap for mankind: ");
-//        Serial.println(num_steps);
+        // Serial.print("One small step for Man, one large leap for mankind: ");
+        // Serial.println(num_steps);
       }
       else if (step_displacement * sign_origin > 0) {
         sign_origin *= -1;
         num_steps += 1;
-//        Serial.print("Number of steps: ");
-//        Serial.println(num_steps);
-//        Serial.print("Distance is: ");
-//        Serial.println(step_displacement);
+        // Serial.print("Number of steps: ");
+        // Serial.println(num_steps);
+        // Serial.print("Distance is: ");
+        // Serial.println(step_displacement);
       }  
     } 
   }
-  delay(50);
+  mqttPublish(writeChannelID, sensorValue, num_steps);
+  delay(500);
+}
+
+void mqttPublish(long pubChannelID, double EMG, int steps) {
+  String topic1, topic2, data1, data2;
+  topic1 = "channels/" + String(pubChannelID) + "/publish";
+  data1 = "field1=" + String(EMG);
+  topic2 = "channels/" + String(pubChannelID) + "/publish";
+  data2 = "field2=" + String(steps);
+  Serial.println(data1);
+  Serial.println(data2);
+  mqttClient.publish(topic1.c_str(), data1.c_str());
+  mqttClient.publish(topic2.c_str(), data2.c_str());
 }
