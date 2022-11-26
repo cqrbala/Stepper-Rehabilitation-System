7e7dcdee84daeb9ff0c50c2a91a863b467da0636
arnavmago <arnavmago@gmail.com>
2022-11-27 01:52:59 +0530

    Initial dashboard designed

--- om2m.py
+++ om2m.py
@@ -3,14 +3,14 @@ uri_cse = "http://127.0.0.1:8080/~/in-cse/in-name"
 ae = "Stepper Data"
 cnt = "node1"
 
-create_ae(uri_cse, ae)
+# create_ae(uri_cse, ae)
 
-uri_ae = uri_cse + "/" + ae
-create_cnt(uri_ae, cnt)
+# uri_ae = uri_cse + "/" + ae
+# create_cnt(uri_ae, cnt)
 
-uri_cnt = uri_ae + "/" + cnt
-create_data_cin(uri_cnt, "ESW")
+# uri_cnt = uri_ae + "/" + cnt
+# create_data_cin(uri_cnt, "ESW")
 
-# delete(uri_cse, "FSR")
-# delete(uri_cse, "Number_Of_Steps")
-# delete(uri_cse, "EMG ")
+delete(uri_cse, "FSR")
+delete(uri_cse, "Number_Of_Steps")
+delete(uri_cse, "EMG ")
