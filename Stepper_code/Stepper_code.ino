19001044c67e60b168b223df1e8a1ce40ce18f6a
arnavmago <arnavmago@gmail.com>
2022-11-26 22:58:07 +0530

    Changed OneM2M uploading format

--- Stepper_code/Stepper_code.ino
+++ Stepper_code/Stepper_code.ino
@@ -5,7 +5,6 @@
 #include <ArduinoJson.h>
 #include "HTTPClient.h"
 
-
 // Defining the pins we will be using to take inputs from the sensors we are using
 #define trigger_pin 33
 #define echo_pin 32
@@ -19,9 +18,9 @@ char ssid[] = "Arnav";
 char password[] = "pranjal13";
 
 // All variables required to connect and upload data to ThingSpeak
-const char* server = "mqtt3.thingspeak.com";
+const char *server = "mqtt3.thingspeak.com";
 char mqttUserName[] = "mwa0000026745613";
-char mqttPass[] = "THZAMM30249U49OK"; //Change to your MQTT API key from Account > MyProfile on Thingspeak.
+char mqttPass[] = "THZAMM30249U49OK"; // Change to your MQTT API key from Account > MyProfile on Thingspeak.
 int writeChannelID = 1925729;
 char writeAPIKey[] = "OLVQ0WA9GL4CLREE";
 char readAPIKey[] = "M5SLOVSH2YRWMURX";
@@ -30,14 +29,16 @@ char readAPIKey[] = "M5SLOVSH2YRWMURX";
 String cse_ip = "192.168.199.15"; // YOUR IP from ipconfig/ifconfig
 String cse_port = "8080";
 String om2mserver = "http://" + cse_ip + ":" + cse_port + "/~/in-cse/in-name/";
-String ae1 = "Number_Of_Steps";
-String cnt1 = "node1";
-String ae2 = "EMG";
-String cnt2 = "node1";
-String ae3 = "FSR";
-String cnt3 = "node1";
+String ae = "Stepper Data";
+String cnt = "node1";
+// String ae1 = "Number_Of_Steps";
+// String cnt1 = "node1";
+// String ae2 = "EMG";
+// String cnt2 = "node1";
+// String ae3 = "FSR";
+// String cnt3 = "node1";
 
-WiFiClient client; // Instantiating WiFiClient object
+WiFiClient client;                             // Instantiating WiFiClient object
 PubSubClient mqttClient(server, 1883, client); // Instantiating Publisher Sub-Client Object
 
 // inital positon of the step which we will use as a point of reference
@@ -47,7 +48,7 @@ double origin;
 int origin_flag = 0;
 
 // Step counter
-int num_steps = 0; 
+int num_steps = 0;
 
 // Keeps track of which side of the origin the step is present (Prevents a single step being counted more than once)
 int sign_origin = 0;
@@ -60,30 +61,32 @@ double step_threshold = 0.05;
 
 double duration, distance, step_displacement, sensorValue, millivolt;
 
-void setup() {
+void setup()
+{
   pinMode(trigger_pin, OUTPUT);
   pinMode(echo_pin, INPUT);
   WiFi.begin(ssid, password);
   Serial.begin(115200);
-  
- while (WiFi.status() != WL_CONNECTED)
- {
-   delay(500);
-   Serial.println("Not connected to WiFi.");
- }
- Serial.println("WiFi connected.");
- Serial.println("IP address: ");
- Serial.println(WiFi.localIP());
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Not connected to WiFi.");
+  }
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   // Setting up MQTT Broker details
   mqttClient.setServer(server, 1883);
 }
 
-void loop()  {
-  while(mqttClient.connected() == NULL)
+void loop()
+{
+  while (mqttClient.connected() == NULL)
   {
     Serial.println("MQTT not connected");
-    Serial.println(mqttClient.connect("KSYxFSI0BicuKikKEyYaEjU","KSYxFSI0BicuKikKEyYaEjU","Na4N9kkuVLSjuO1xucSvUd60"));
+    Serial.println(mqttClient.connect("KSYxFSI0BicuKikKEyYaEjU", "KSYxFSI0BicuKikKEyYaEjU", "Na4N9kkuVLSjuO1xucSvUd60"));
   }
   Serial.println("MQTT connected");
   mqttClient.loop(); // maintain connection
@@ -92,19 +95,21 @@ void loop()  {
   delayMicroseconds(10);
   digitalWrite(trigger_pin, 0);
 
-  if (origin_flag == 0) {
+  if (origin_flag == 0)
+  {
     // origin point is being set
     origin = pulseIn(echo_pin, 1);
-    origin = (origin / 2000000)*sound_speed;
+    origin = (origin / 2000000) * sound_speed;
     origin_flag = 1;
     Serial.print("Origin set as: ");
     Serial.println(origin);
   }
-  else {
-    // Processing of the ultrasonic sensor's input    
+  else
+  {
+    // Processing of the ultrasonic sensor's input
     duration = pulseIn(echo_pin, 1);
-    duration = duration / 2000000; 
-    distance = duration*sound_speed;
+    duration = duration / 2000000;
+    distance = duration * sound_speed;
 
     Serial.print("Ultrasonic sensor data is: ");
     Serial.println(distance);
@@ -112,7 +117,7 @@ void loop()  {
     // Reading EMG sensor input (gets the voltage in milliVolts)
     sensorValue = analogRead(EMG_Sensor_pin);
     sensorValue = analogRead(EMG_Sensor_pin);
-    millivolt = (sensorValue/1023)*5;
+    millivolt = (sensorValue / 1023) * 5;
 
     Serial.print("Muscle sensor value: ");
     Serial.println(sensorValue);
@@ -123,41 +128,49 @@ void loop()  {
     value = analogRead(Sensor_pin);
     Serial.print("sensor value is: ");
     Serial.println(value);
-    
+
     step_displacement = distance - origin;
     Serial.print("Distance is: ");
     Serial.println(step_displacement);
-    
-    if(abs(step_displacement) >= step_threshold) {
-      if(sign_flag ==0) {
+
+    if (abs(step_displacement) >= step_threshold)
+    {
+      if (sign_flag == 0)
+      {
         sign_flag = 1;
-        if(step_displacement < 0) sign_origin = 1;
-        else sign_origin = -1;
+        if (step_displacement < 0)
+          sign_origin = 1;
+        else
+          sign_origin = -1;
         num_steps += 1;
         Serial.print("One small step for Man, one large leap for mankind: ");
         Serial.println(num_steps);
       }
-      else if (step_displacement * sign_origin > 0) {
+      else if (step_displacement * sign_origin > 0)
+      {
         sign_origin *= -1;
         num_steps += 1;
         Serial.print("Number of steps: ");
         Serial.println(num_steps);
         Serial.print("Distance is: ");
         Serial.println(step_displacement);
-      }  
-    } 
+      }
+    }
   }
   String value_str = String(value);
   String num_steps_str = String(num_steps);
   String sensorValue_str = String(sensorValue);
+  String Concatenated = value_str + "," + num_steps_str + "," + sensorValue_str;
   mqttPublish(writeChannelID, sensorValue, num_steps, value);
-  createCISteps(num_steps_str);
-  createCIEMG(sensorValue_str);
-  createCIFSR(value_str);
+  createCIStepper(Concatenated);
+  // createCISteps(num_steps_str);
+  // createCIEMG(sensorValue_str);
+  // createCIFSR(value_str);
   delay(500);
 }
 
-void mqttPublish(long pubChannelID, double EMG, int steps, int FSR) {
+void mqttPublish(long pubChannelID, double EMG, int steps, int FSR)
+{
   String topic1, topic2, data1, data2, topic3, data3;
   topic1 = "channels/" + String(pubChannelID) + "/publish";
   data1 = "field1=" + String(EMG);
@@ -173,10 +186,10 @@ void mqttPublish(long pubChannelID, double EMG, int steps, int FSR) {
   mqttClient.publish(topic3.c_str(), data3.c_str());
 }
 
-void createCISteps(String &val)
+void createCIStepper(String &val)
 {
   HTTPClient http;
-  http.begin(om2mserver + ae1 + "/" + cnt1 + "/");
+  http.begin(om2mserver + ae + "/" + cnt + "/");
 
   http.addHeader("X-M2M-Origin", "admin:admin");
   http.addHeader("Content-Type", "application/json;ty=4");
@@ -184,42 +197,63 @@ void createCISteps(String &val)
   int code = http.POST("{\"m2m:cin\": {\"cnf\":\"application/json\",\"con\": " + String(val) + "}}");
 
   Serial.println(code);
-  if (code == -1) {
+  if (code == -1)
+  {
     Serial.println("UNABLE TO CONNECT TO THE SERVER");
   }
   http.end();
 }
 
-void createCIEMG(String &val)
-{
-  HTTPClient http;
-  http.begin(om2mserver + ae2 + "/" + cnt2 + "/");
+// void createCISteps(String &val)
+// {
+//   HTTPClient http;
+//   http.begin(om2mserver + ae1 + "/" + cnt1 + "/");
 
-  http.addHeader("X-M2M-Origin", "admin:admin");
-  http.addHeader("Content-Type", "application/json;ty=4");
+//   http.addHeader("X-M2M-Origin", "admin:admin");
+//   http.addHeader("Content-Type", "application/json;ty=4");
 
-  int code = http.POST("{\"m2m:cin\": {\"cnf\":\"application/json\",\"con\": " + String(val) + "}}");
+//   int code = http.POST("{\"m2m:cin\": {\"cnf\":\"application/json\",\"con\": " + String(val) + "}}");
 
-  Serial.println(code);
-  if (code == -1) {
-    Serial.println("UNABLE TO CONNECT TO THE SERVER");
-  }
-  http.end();
-}
+//   Serial.println(code);
+//   if (code == -1)
+//   {
+//     Serial.println("UNABLE TO CONNECT TO THE SERVER");
+//   }
+//   http.end();
+// }
 
-void createCIFSR(String &val)
-{
-  HTTPClient http;
-  http.begin(om2mserver + ae3 + "/" + cnt3 + "/");
+// void createCIEMG(String &val)
+// {
+//   HTTPClient http;
+//   http.begin(om2mserver + ae2 + "/" + cnt2 + "/");
 
-  http.addHeader("X-M2M-Origin", "admin:admin");
-  http.addHeader("Content-Type", "application/json;ty=4");
+//   http.addHeader("X-M2M-Origin", "admin:admin");
+//   http.addHeader("Content-Type", "application/json;ty=4");
 
-  int code = http.POST("{\"m2m:cin\": {\"cnf\":\"application/json\",\"con\": " + String(val) + "}}");
+//   int code = http.POST("{\"m2m:cin\": {\"cnf\":\"application/json\",\"con\": " + String(val) + "}}");
 
-  Serial.println(code);
-  if (code == -1) {
-    Serial.println("UNABLE TO CONNECT TO THE SERVER");
-  }
-  http.end();
-}
+//   Serial.println(code);
+//   if (code == -1)
+//   {
+//     Serial.println("UNABLE TO CONNECT TO THE SERVER");
+//   }
+//   http.end();
+// }
+
+// void createCIFSR(String &val)
+// {
+//   HTTPClient http;
+//   http.begin(om2mserver + ae3 + "/" + cnt3 + "/");
+
+//   http.addHeader("X-M2M-Origin", "admin:admin");
+//   http.addHeader("Content-Type", "application/json;ty=4");
+
+//   int code = http.POST("{\"m2m:cin\": {\"cnf\":\"application/json\",\"con\": " + String(val) + "}}");
+
+//   Serial.println(code);
+//   if (code == -1)
+//   {
+//     Serial.println("UNABLE TO CONNECT TO THE SERVER");
+//   }
+//   http.end();
+// }
--- om2m.py
+++ om2m.py
@@ -1,26 +1,16 @@
 from onem2m_func import *
 uri_cse = "http://127.0.0.1:8080/~/in-cse/in-name"
-ae1 = "FSR"
-cnt1 = "node1"
-ae2 = "Number_Of_Steps"
-cnt2 = "node1"
-ae3 = "EMG"
-cnt3 = "node1"
+ae = "Stepper Data"
+cnt = "node1"
 
-create_ae(uri_cse, ae1)
-create_ae(uri_cse, ae2)
-create_ae(uri_cse, ae3)
+create_ae(uri_cse, ae)
 
-uri_ae1 = uri_cse + "/" + ae1
-create_cnt(uri_ae1, cnt1)
-uri_ae2 = uri_cse + "/" + ae2
-create_cnt(uri_ae2, cnt2)
-uri_ae3 = uri_cse + "/" + ae3
-create_cnt(uri_ae3, cnt3)
+uri_ae = uri_cse + "/" + ae
+create_cnt(uri_ae, cnt)
 
-uri_cnt1 = uri_ae1 + "/" + cnt1
-create_data_cin(uri_cnt1, "ESW FSR")
-uri_cnt2 = uri_ae2 + "/" + cnt2
-create_data_cin(uri_cnt2, "ESW FSR")
-uri_cnt3 = uri_ae3 + "/" + cnt3
-create_data_cin(uri_cnt3, "ESW FSR")
+uri_cnt = uri_ae + "/" + cnt
+create_data_cin(uri_cnt, "ESW")
+
+# delete(uri_cse, "FSR")
+# delete(uri_cse, "Number_Of_Steps")
+# delete(uri_cse, "EMG ")
